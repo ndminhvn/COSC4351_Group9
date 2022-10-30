c76420f503a01479c7f85b33e39a55d864c33a59
Quang Du <qdtheta@gmail.com>
2022-10-30 17:15:51 -0500

    Sync

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,24 @@
 const express = require("express")
-let router = express.Router()
+const router = express.Router()
+const User = require("../models/User")
 
-router.route("/register")
-.get((req,res) => {
-    res.send("register")
+// "/user/register"
+router.post("/register", async (req,res) => {
+    const user = new User({
+        FirstName: req.body.FirstName,
+        LastName: req.body.LastName,
+        PhoneNumber: req.body.PhoneNumber,
+        Password: req.body.Password
+    })
+    try {
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    } catch (error) {
+        res.status(400).json({error})
+    }
 })
+
+
 
 router.route("/login")
 .get((req,res) => {
--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
-const userRoute = require("./routes/user")
 
 
 
+//Connect to MongoDB
 const dbURI = "mongodb+srv://admin:group9@database.b6uwacv.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((result) => console.log("Connected to database"))
     .catch((err) => console.log(err));
 
+//Routes
+const userRoute = require("./routes/user")
 app.use("/user", userRoute)
 //use register.js file to handle
 //endpoints that start with /register
