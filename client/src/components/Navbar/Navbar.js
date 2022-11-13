2fee4bda026f62f5b19fa6d76aca3409dbdbef7c
Minh Nguyen <vn.ndminh@gmail.com>
2022-11-12 23:12:21 -0600

    Update Navbar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Container, Image, Nav, Navbar } from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import logo from '../../assets/logo.webp';
+import './Navbar.css'
+
+const NavBar = () => {
+  return (
+	<div className='main-nav'>
+		<Navbar>
+			<Container>
+				<Navbar.Brand>
+					{/* <Nav.Link href={`${window.location.protocol}//${window.location.host}`}> */}
+					<Nav.Link href='/'>
+						<Image fluid roundedCircle src={logo} className='App-logo shadow-sm' alt='logo' />
+					</Nav.Link>
+				</Navbar.Brand>
+			</Container>
+			{/* <Navbar.Toggle /> */}
+
+			<Navbar.Collapse className='justify-content-end'>
+					<Button variant='contained' href='/login' id='nav-btn' size='lg'>
+						Login/Register
+					</Button>
+			</Navbar.Collapse>
+		</Navbar>
+
+		<Outlet />
+	</div>
+	)
+};
+
+export default NavBar;
