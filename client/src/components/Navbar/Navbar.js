9b477d92e78b85f7f131c86078c43432984c585b
Minh Nguyen <vn.ndminh@gmail.com>
2022-11-14 23:45:57 -0600

    Re-design nav bar with new logo, use link for login with hover animation instead of button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container, Image, Nav, Navbar } from 'react-bootstrap';
 import Button from '@mui/material/Button';
-import logo from '../../assets/logo.webp';
+import Avatar from '@mui/material/Avatar';
+// import logo from '../../assets/logo.webp';
+import logo from '../../assets/logo1.png';
 import './Navbar.css'
+import { Link } from '@mui/material';
 
 const NavBar = () => {
   return (
@@ -13,17 +16,23 @@ const NavBar = () => {
 				<Navbar.Brand>
 					{/* <Nav.Link href={`${window.location.protocol}//${window.location.host}`}> */}
 					<Nav.Link href='/'>
-						<Image fluid roundedCircle src={logo} className='App-logo shadow-sm' alt='logo' />
-            <b>Sample Restaurant</b>
+						<Image fluid 
+						className='App-logo shadow-sm' alt='logo' 
+						src={logo} 
+						/>
+            			{/* <b>Sample</b> */}
 					</Nav.Link>
 				</Navbar.Brand>
 			</Container>
 			{/* <Navbar.Toggle /> */}
 
-			<Navbar.Collapse className='justify-content-end'>
+			{/* <Navbar.Collapse className='justify-content-end'>
 					<Button variant='contained' href='/login' id='nav-btn' size='lg'>
 						Login/Register
 					</Button>
+			</Navbar.Collapse> */}
+			<Navbar.Collapse className='justify-content-end'>
+				<Link href='/login' id='nav-link' underline='none'>Login</Link>
 			</Navbar.Collapse>
 		</Navbar>
 
