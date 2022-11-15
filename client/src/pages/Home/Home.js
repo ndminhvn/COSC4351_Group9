dddc3edff605a99305d716e38860c94ed2c0d2df
Minh Nguyen <vn.ndminh@gmail.com>
2022-11-14 23:45:12 -0600

    re-design the homepage with new background, logo and fonts

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,26 @@
 import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Divider from '@mui/material/Divider';
+
+import bgVideo from '../../assets/videobg.mp4';
 import './Home.css';
+import { Typography } from '@mui/material';
 
 const Home = () => {
     return (
+        <>
+        <video autoPlay muted loop id='bgVideo'>
+            <source src={bgVideo} type='video/mp4' />
+        </video>
         <div id='home'>
-            <h1>Sample Restaurant</h1>
+            <Divider style={{ marginLeft:'30%', width:'600px' }} variant='dynamic' component="div" role="presentation">
+                <Typography color={'whitesmoke'} letterSpacing='0.25rem' variant="h5">WE ARE</Typography>
+            </Divider>
+            <h1>Team 9 Kitchen</h1>
             <div className='h-50 d-flex flex-column align-items-center justify-content-around btn-group'>
                 <Button variant='contained' href='/reserve' id='reserve-btn' className='mb-5' size='lg'>
                     Book a table now
+                    <ArrowForwardIcon id='arrow-icon' sx={{ mr: 1 }} />
                 </Button>
                 <span><p><i>Already reserved a table?</i></p>
                     <Button variant='contained' href='/detail' id='check-btn' size='lg'>
@@ -16,6 +29,7 @@ const Home = () => {
                 </span>
             </div>
         </div>
+        </>
     )
 }
 
