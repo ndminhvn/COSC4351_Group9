4df732b037b671ca8fd1b20ee79505269849056c
Quang Du <qdtheta@gmail.com>
2022-11-27 01:46:10 -0600

    Change phonenumber to string

--- server/models/User.js
+++ server/models/User.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
         required: true
     },
     phoneNumber: {
-        type: Number,
+        type: String,
         required: true,
         unique: true
     },
