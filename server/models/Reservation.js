7a5eb68c76cb0273a94dc79fc86e3acfd0e96136
Quang Du <qdtheta@gmail.com>
2022-11-29 01:03:58 -0600

    Sync

--- server/models/Reservation.js
+++ server/models/Reservation.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 
 let reservationSchema = new mongoose.Schema({
-    phoneNumber:{
-       type: String,
-       required: true
-    }
-});
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    creditCard: {
+        cardNumber: String,
+        expDate: String,
+        cvv: String
+        }                 
+    });
 
 let Reservation = mongoose.model("Reservation", reservationSchema);
 
